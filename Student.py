6db294420af8937d54d31fe42e4490ed8b77ad70
lindsawi <59423239+lindsawi@users.noreply.github.com>
2020-02-14 12:06:25 -0600

    Create Student.py
    Strings #2

---
+++ Student.py
@@ -0,0 +1,16 @@
+# Fix the student mistake
+# Lindsay Williams 
+# Feb 12, 2020
+
+# import modules
+# Add varaibles
+# Ask for input
+# Make it work
+# print results
+
+###################
+string1 = "I am a student"
+string2 = "in the biology department."
+string3 = string1 + " " + string2
+print(string3)
+####################
